824012c8a195c827ceade327b1c65f5dc8e1b762
youssef <youssefmsaadi95@gmail.com>
2020-03-02 17:45:44 +0100

    initial commit

---
+++ src/Personne.java
@@ -0,0 +1,17 @@
+public class Personne {
+    private String nom;
+    private int age;
+
+    public Personne(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
